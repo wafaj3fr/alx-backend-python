3748399b8b0586da92a0ac7440f538463e216282
wafaj3fr <wafaagaafar@gmail.com>
2024-08-01 22:10:11 +0000

    a-z

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - floor
+"""
+
+
+def floor(n: float) -> int:
+    """
+    type-annotated function floor
+    """
+    return int(n)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - to string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    type-annotated function to_str
+    """
+    return str(n)
