28e489a5a700026a296773adbd2682351fe2e898
wafaj3fr <wafaagaafar@gmail.com>
2024-08-01 21:35:53 +0000

    1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    type-annotated function concat
+    """
+    return str1 + str2
